03d6fa4fb6968dda773218bbab6b3721240fda97
JuandaPatra <patrajuanda10@gmail.com>
2022-01-25 21:26:37 +0700

    first-init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,93 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div class="container ws">
+  <h2>To-Do App</h2>
+  <div class="d-flex">
+  <div class="input-group mb-3">
+    <input type="text" v-model="inputActivities" class="form-control" placeholder="Activities" aria-label="Username" aria-describedby="basic-addon1">
   </div>
+  <button @click="submit" class="btn btn-warning rounded-0 tombol">Add</button>
+  </div>
+
+  <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Activities</th>
+      <th scope="col">Progress</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(item,index) in task" :key="index+1">
+      <th scope="row">{{index+1}}</th>
+      <td>{{item.name}}</td>
+      <td>{{item.status}}</td>
+      <td>
+        <div @click="deleteTask(item.id)" >
+          Delete
+          </div> 
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+
+</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'To-Do-App',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      inputActivities : '',
+      task : [
+        {
+          id:1,
+          name : 'playing tennis',
+          status : 'on-progress'
+        },
+        {
+          id:2,
+          name : 'Eating Banana',
+          status : 'to-do'
+        },
+      ]
+    }
+  },
+  methods :{
+    submit(){
+      this.task.push({
+        id:this.task.length+1,
+        name:this.inputActivities,
+        status: 'to-do'
+      })
+    },
+    deleteTask(index){
+      console.log(index)
+      let newIndex = index
+      let result =this.task.filter(item=>item.id !== newIndex)
+      // console.log(result)
+      this.task= [...result]
+
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+.ws{
+  background-color: #42b983;
+  width: 600px;
+  height: 400px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.tombol{
+  height: 40px;
 }
 </style>
